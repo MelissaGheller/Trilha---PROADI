2df3f15751ec228227d32da437d0ab3c516e3e84
Sylviohmv <sylvio.vaz@hmv.org.br>
2025-02-27 10:04:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -30,9 +30,9 @@ const casas = [
   /*13*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Gestão Hospitalar RS é focado exclusivamente na gestão financeira de hospitais?", resposta: false, explicacao: "O projeto abrange aspectos administrativos e assistenciais, não apenas financeiros.", tipo: "pergunta" },
 
 
-  /*14*/ { background:"img/fundo/projeto-lean-nas-emergencias.png", tipo: "projeto"  },
+  /*14*/ { background:"img/fundo/projeto-saude-em-nossa-maos.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=KXgYEJYcOeQ",  tipo: "projeto"  },
 
-  /*15*/ {background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto LEANS é focado exclusivamente na gestão financeira de hospitais?", resposta: false, explicacao: "O projeto abrange aspectos administrativos e assistenciais, não apenas financeiros.", tipo: "pergunta" },
+  /*15*/ {background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Saúde em nossas Mãos trata de agricultura familiar?", resposta: false, explicacao: "O projeto foca na segurança e qualidade no atendimento hospitalar.", tipo: "pergunta" },
 
   /*16*/ { background:"img/fundo/projeto-cardiopatia-congenita.png",video:"https://www.youtube.com/watch?v=he8cB1eDdMM",  tipo: "projeto"  },
   
@@ -44,88 +44,87 @@ const casas = [
   /*17*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "cardiopatia congenita?", resposta: true, explicacao: "cardiopatia congenita.", tipo: "pergunta" },
 
 
-  /*18*/ { background:"img/fundo/projeto-saude-em-nossa-maos.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=KXgYEJYcOeQ",  tipo: "projeto"  },
+  /*18*/ { background: "img/fundo/projeto-ats-educação.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=7mvu2ebAU8E",  tipo: "projeto"},
 
-  /*19*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto ATS Educação capacita gestores apenas para gestão financeira?", resposta: false, explicacao: "O projeto aborda avaliação de tecnologias em saúde e não se limita à gestão financeira.", tipo: "pergunta" },
+  /*19*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "Diretrizes bla bla bla?", resposta: false, explicacao: "O estudo é direcionado para doenças crônicas, não agudas.", tipo: "pergunta" },
 
-  /*20*/ { background: "img/fundo/projeto-ats-educação.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=7mvu2ebAU8E",  tipo: "projeto"},
+  /*20*/ { background:"img/fundo/projeto-diretrizes.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=oNKqp0AP9_o",  tipo: "projeto"  },
 
   /*23*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Preceptoria Médica forma apenas profissionais de enfermagem?", resposta: false, explicacao: "O projeto é voltado à formação de novos médicos.", tipo: "pergunta" },
 
   /*22*/ { background:"img/fundo/projeto-preceptoria-medica.png",titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=WA6BLGVNN14",  tipo: "projeto"  },
 
 
-  /*19*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Saúde em nossas Mãos trata de agricultura familiar?", resposta: false, explicacao: "O projeto foca na segurança e qualidade no atendimento hospitalar.", tipo: "pergunta" },
+  /*19*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto ATS Educação capacita gestores apenas para gestão financeira?", resposta: false, explicacao: "O projeto aborda avaliação de tecnologias em saúde e não se limita à gestão financeira.", tipo: "pergunta" },
 
   /*V*/ { titulo: "", conteudo: "", tipo: "" },
   /*V*/ { titulo: "", conteudo: "", tipo: "" },
   /*V*/ { titulo: "", conteudo: "", tipo: "" },
   /*V*/ { titulo: "", conteudo: "", tipo: "" },
 
-  /*22*/ { background:"img/fundo/projeto-preceptoria-mfc.png", titulo: "", conteudo: "", video: "/img/videoteste.mp4",  tipo: "projeto"  },
+  /*22*/ { background:"img/fundo/projeto-artemis.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=X2WEOnF5mao",  tipo: "projeto"  },
 
-  /*25*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto MFC bla bla bla?", resposta: false, explicacao: "O projeto é voltado à formação de novos médicos.", tipo: "pergunta" },
-
+  /*25*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Artemis está relacionado à transformação digital na saúde?", resposta: true, explicacao: "O projeto Artemis promove transformação digital na saúde.", tipo: "pergunta" },
   
-  /*22*/ { background:"img/fundo/projeto-preceptoria-multiprofissional.png", titulo: "", conteudo: "", video: "/img/videoteste.mp4",  tipo: "projeto" },
+  /*22*/ { background:"img/fundo/projeto-estudo-otimo.png", titulo: "", conteudo: "", video: "/img/videoteste.mp4",  tipo: "projeto"  },
 
-  /*27*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Preceptoria Multiprofissional envolve apenas médicos?", resposta: false, explicacao: "O projeto promove a formação em equipes multiprofissionais.", tipo: "pergunta" },
+  /*27*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "Estudo otimo alterar pergunta?", resposta: true, explicacao: "O projeto Artemis promove transformação digital na saúde.", tipo: "pergunta" },
 
-  /*28*/ {background:"img/fundo/projeto-saude-indigena.png",  tipo: "projeto"  },
-
-  /*V*/ { titulo: "", conteudo: "", tipo: "" },
-  /*V*/ { titulo: "", conteudo: "", tipo: "" },
-  /*V*/ { titulo: "", conteudo: "", tipo: "" },
-  /*V*/ { titulo: "", conteudo: "", tipo: "" },
-
-  /*29*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Saúde Indígena busca integrar as tradições indígenas ao atendimento em saúde?", resposta: true, explicacao: "O projeto promove ações específicas para populações indígenas, respeitando suas tradições.", tipo: "pergunta" },
-
-
-  /*30*/ {background:"img/fundo/projeto-artemis.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=X2WEOnF5mao",  tipo: "projeto"  },
-
-  /*31*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "Diretrizes bla bla bla?", resposta: false, explicacao: "O estudo é direcionado para doenças crônicas, não agudas.", tipo: "pergunta" },
-
-  /*32*/ {background:"img/fundo/projeto-diretrizes.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=oNKqp0AP9_o",  tipo: "projeto"  },
-
-  /*33*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O Estudo CuidAR é voltado para doenças respiratórias agudas?", resposta: false, explicacao: "O estudo é direcionado para doenças crônicas, não agudas.", tipo: "pergunta" },
-
-  /*34*/ {background:"img/fundo/projeto-estudo-cuidar.png", titulo: "", conteudo: "",  tipo: "projeto"  },
-
-  /*31*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Artemis está relacionado à transformação digital na saúde?", resposta: true, explicacao: "O projeto Artemis promove transformação digital na saúde.", tipo: "pergunta" },
+  /*28*/ {background:"img/fundo/projeto-onco-genomas.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=wOkTcLAxYPQ",  tipo: "projeto"  },
 
   /*V*/ { titulo: "", conteudo: "", tipo: "" },
   /*V*/ { titulo: "", conteudo: "", tipo: "" },
   /*V*/ { titulo: "", conteudo: "", tipo: "" },
   /*V*/ { titulo: "", conteudo: "", tipo: "" },
 
-  /*19*/ {background:"img/fundo/projeto-estudo-prisma.png", titulo: "", conteudo: "",  tipo: "projeto"  },
+  /*29*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Onco-Genomas Brasil analisa tratamentos personalizados para câncer?", resposta: true, explicacao: "O projeto estuda o perfil genético de cânceres no Brasil.", tipo: "pergunta" },
 
-  /*31*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "Estudo HTLV (PRISMA) alerar pergunta?", resposta: true, explicacao: "O projeto Artemis promove transformação digital na saúde.", tipo: "pergunta" },
 
-  /*19*/{background:"img/fundo/projeto-estudo-otimo.png", titulo: "", conteudo: "", video: "/img/videoteste.mp4",  tipo: "projeto"  },
+  /*30*/ {background:"img/fundo/projeto-rehab-vm.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=KUPtzfuNgH0",  tipo: "projeto"  },
 
-  /*31*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "Estudo otimo alerar pergunta?", resposta: true, explicacao: "O projeto Artemis promove transformação digital na saúde.", tipo: "pergunta" },
+  /*31*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Pós-Covid Brasil estuda apenas os impactos físicos da Covid-19?", resposta: false, explicacao: "O estudo avalia os impactos físicos, mentais e sociais.", tipo: "pergunta" },
 
-  /*19*/ {background:"img/fundo/projeto-onco-genomas.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=wOkTcLAxYPQ",  tipo: "projeto"  },
+  /*32*/ {background:"img/fundo/projeto-pos-covid.png", titulo: "", conteudo: "",  video: "/img/videoteste.mp4",  tipo: "projeto" }, 
+  
+  /*33*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "colocar pergunta sobre o Projeto PhysioSync-HF Follow-UP?", resposta: true, explicacao: "colocar resposta Projeto PhysioSync-HF Follow-UP.", tipo: "pergunta" },
+
+  /*34*/ {background:"img/fundo/projeto-physiosync.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=Atw5AdfzTm8",  tipo: "projeto"  },
+
+  /*31*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Rehab-VM Brasil é voltado à reabilitação de pacientes com doenças respiratórias leves?", resposta: false, explicacao: "O projeto foca na reabilitação de pacientes em ventilação mecânica prolongada.", tipo: "pergunta" },
 
   /*V*/ { titulo: "", conteudo: "", tipo: "" },
   /*V*/ { titulo: "", conteudo: "", tipo: "" },
   /*V*/ { titulo: "", conteudo: "", tipo: "" },
   /*V*/ { titulo: "", conteudo: "", tipo: "" },
 
-  /*39*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Onco-Genomas Brasil analisa tratamentos personalizados para câncer?", resposta: true, explicacao: "O projeto estuda o perfil genético de cânceres no Brasil.", tipo: "pergunta" },
+  /*19*/ {background:"img/fundo/projeto-resilient.png", titulo: "", conteudo: "", video: "/img/videoteste.mp4",  tipo: "projeto"  },
+
+  /*31*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O Projeto Resilient-TNK alterar pergunta?", resposta: false, explicacao: "O projeto foca na reabilitação de pacientes em ventilação mecânica prolongada.", tipo: "pergunta" },
+
+  /*19*/{background:"img/fundo/projeto-tb-ped.png", titulo: "", conteudo: "", video: "/img/videoteste.mp4",  tipo: "projeto"  },
+  
+  /*31*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto TB PED é voltado ao tratamento da tuberculose pediátrica?", resposta: true, explicacao: "O projeto foca no diagnóstico e tratamento da tuberculose pediátrica.", tipo: "pergunta" },
+
+  /*19*/ {background:"img/fundo/projeto-trident.png", titulo: "", conteudo: "", video: "https://www.youtube.com/watch?v=-OOP8JS5OGQ",  tipo: "projeto"  },
+
+  /*V*/ { titulo: "", conteudo: "", tipo: "" },
+  /*V*/ { titulo: "", conteudo: "", tipo: "" },
+  /*V*/ { titulo: "", conteudo: "", tipo: "" },
+  /*V*/ { titulo: "", conteudo: "", tipo: "" },
+
+  /*39*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Trident é voltado a diagnósticos mais rápidos?", resposta: true, explicacao: "O projeto busca avanços tecnológicos para diagnósticos rápidos e precisos.", tipo: "pergunta" },
 
   /*21*/ {background:"img/fundo/projeto-physiosync.png", titulo: "", conteudo: "", video:"https://www.youtube.com/watch?v=Atw5AdfzTm8",  tipo: "projeto"  },
 
-  /*40*/ {background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto Pós-Covid Brasil estuda apenas os impactos físicos da Covid-19?", resposta: false, explicacao: "O estudo avalia os impactos físicos, mentais e sociais.", tipo: "pergunta" },
+  /*40*/ {background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto VigiaSUS trata de agricultura sustentável?", resposta: false, explicacao: "O projeto é focado na vigilância em saúde e monitoramento de doenças emergentes.", tipo: "pergunta" },
   
-  /*20*/ { background:"img/fundo/projeto-pos-covid.png", titulo: "", conteudo: "",  video: "/img/videoteste.mp4",  tipo: "projeto" },
+  /*20*/ { background:"img/fundo/projeto-vigiasus.png", titulo: "", conteudo: "", video: "https://www.youtube.com/watch?v=ZU4Z985I9iU",  tipo: "projeto"  },
 
 
 
-  /*41*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto RHP aborda padrões de cuidado em redes hospitalares?", resposta: true, explicacao: "O projeto estuda redes hospitalares e padrões de cuidado.", tipo: "pergunta" },
+  /*41*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O projeto TeleNordeste utiliza telemedicina para ampliar o acesso à saúde?", resposta: true, explicacao: "O projeto utiliza telemedicina para expandir o acesso à saúde no Nordeste do Brasil.", tipo: "pergunta" },
 
-  /*22*/ {  background:"img/fundo/projeto-rhp.png", titulo: "", conteudo: "",  tipo: "projeto"  },
+  /*22*/ {  background:"img/fundo/projeto-vigiasus.png", titulo: "", conteudo: "", video: "https://www.youtube.com/watch?v=ZU4Z985I9iU",  tipo: "projeto"  },
 
   /*41*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "colocar pergunta sobre o Projeto PhysioSync-HF Follow-UP?", resposta: true, explicacao: "colocar resposta Projeto PhysioSync-HF Follow-UP.", tipo: "pergunta" },
 
@@ -141,7 +140,7 @@ const casas = [
 
   /*23*/ { background:"img/fundo/projeto-resilient.png", titulo: "", conteudo: "", video: "/img/videoteste.mp4",  tipo: "projeto"  },
 
-  /*47*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O Projeto TNK alterar pergunta?", resposta: false, explicacao: "O projeto foca na reabilitação de pacientes em ventilação mecânica prolongada.", tipo: "pergunta" },
+  /*47*/ { background:"img/fundo/ta-certo-isso.png", pergunta: "O Projeto Resilient-TNK alterar pergunta?", resposta: false, explicacao: "O projeto foca na reabilitação de pacientes em ventilação mecânica prolongada.", tipo: "pergunta" },
 
 
   /*26*/ {background:"img/fundo/projeto-tb-ped.png", titulo: "", conteudo: "", video: "/img/videoteste.mp4",  tipo: "projeto"  },
